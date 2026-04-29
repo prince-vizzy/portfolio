c628fa7a310afffbcdd97b4b8373211d3f2b5d82
prince-vizzy <mwendavictory2@gmail.com>
2026-04-29 20:47:25 +0300

    Add darts app to portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -8,15 +7,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
->>>>>>> 12e07bc573124e378ab29574986787b29ee87cc9
 )
