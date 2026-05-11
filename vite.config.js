277f4e39182dcc1cece2ded4ed15553c4125699e
prince-vizzy <mwendavictory2@gmail.com>
2026-05-11 20:59:59 +0300

    Configure Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: process.env.VERCEL ? '/' : '/portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
